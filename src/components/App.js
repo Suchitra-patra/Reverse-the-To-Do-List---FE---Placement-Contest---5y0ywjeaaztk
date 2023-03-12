e554371714f6ace9a348869d5992ae32528bc834
Suchitra-patra <suchitra3576@gmail.com>
2023-03-12 15:41:59 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -24,12 +24,28 @@ function App() {
     createdAt: '18:00',
   }
   ]);
+  const reverseTodos = () => {
+    setTodos([...todos].reverse());
+  };
 
   return (
     <div id="main">
-      <button>Reverse</button>
-      <table>
+      <button onClick={reverseTodos}>Reverse</button>
+       <table>
         <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.id}>
+              <td>
+                <p>{todo.id}</p>
+              </td>
+              <td>
+                <input />
+              </td>
+              <td>
+                <p>{todo.createdAt}</p>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
